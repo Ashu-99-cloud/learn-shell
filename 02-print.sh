e6929cebd3f684afff20c788e62703e284fb4d52
ashu-99-cloud <ashishujawane1999@gmail.com>
2023-09-10 11:40:35 +0530

    added syntax and information to print the text in colorful format

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,22 @@
 echo Hello World!
+
+# There are 6 colors available in shell
+# Red       - 31
+# Yellow    - 32
+# Green     - 33
+# Blue      - 34
+# Magenta   - 35
+# Cyan      - 36
+
+# Syntax of color print
+# echo -e "\e[COLmMESSAGE\[0m"
+# -e option to enable color
+# \e[COLM - To defined which color
+#\e[0m    - To disable color
+
+echo -e "\e[31mRED COLOR\e[0m"
+echo -e "\e[32mYellow COLOR\e[0m"
+echo -e "\e[33mGreen COLOR\e[0m"
+echo -e "\e[34mBlue COLOR\e[0m"
+echo -e "\e[35mMagenta COLOR\e[0m"
+echo -e "\e[36mCyan COLOR\e[0m"
